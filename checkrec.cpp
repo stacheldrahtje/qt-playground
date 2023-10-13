d11b35e169e1ed2ab8cff6726da85ad0d51357e8
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-10-13 06:50:03 +0200

    add class checkrec

---
+++ checkrec.cpp
@@ -0,0 +1,6 @@
+#include "checkrec.h"
+
+checkrec::checkrec()
+{
+
+}
---
+++ checkrec.h
@@ -0,0 +1,11 @@
+#ifndef CHECKREC_H
+#define CHECKREC_H
+
+
+class checkrec
+{
+public:
+    checkrec();
+};
+
+#endif // CHECKREC_H
