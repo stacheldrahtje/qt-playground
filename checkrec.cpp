fe981abadb52eace7f2254c38e4428e978e3d684
Natasha Ament-Teusink <stacheldrahtje@gmail.com>
2023-10-13 10:52:50 +0200

    Basis of regex in Qt

--- checkrec.cpp
+++ checkrec.cpp
@@ -1,6 +1,20 @@
 #include "checkrec.h"
+#include <QDebug>
+#include <QString>
+#include <QRegularExpression>
 
 checkrec::checkrec()
 {
+    static QRegularExpression re("Dat");
+    QString dat = "Dit";
+    QRegularExpressionMatch match = re.match(dat);
+    if (match.hasMatch() == true)
+    {
+        qDebug() << "Match";
+    }
+    else
+    {
+        qDebug() << "No match";
+    }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
+#include "checkrec.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    checkrec();
     return a.exec();
 }
